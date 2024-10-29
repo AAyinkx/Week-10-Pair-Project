49792297cbb2c8989d0c3402c067b70098b6bfd8
AAyinkx <ayinkafinowi@gmail.com>
2024-10-29 09:41:36 +0000

    Displayed on page successfully

--- src/components/figletToPage.jsx
+++ src/components/figletToPage.jsx
@@ -5,36 +5,9 @@ import figlet from "figlet";
 import { useAsciiText, alphabet } from "react-ascii-text";
 
 export default function FigletToPage() {
-  function FizzBuzz(number) {
-    //Creates Array fror the  numbers 1 --> Target Number
-    let array1 = [];
-    let array2 = [];
-    //Creates Array fror the  numbers 1 --> Target Number
-    for (let i = 1; i < number + 1; i++) {
-      array1.push(i);
-    }
-    //Checks conditions for each number in array of numbers
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] % 5 === 0 && array1[i] % 3 === 0) {
-        array2.push("FizzBuzz");
-      } else if (array1[i] % 3 === 0) {
-        array2.push("Fizz");
-      } else if (array1[i] % 5 === 0) {
-        array2.push("Buzz");
-      } else {
-        array2.push(array1[i].toString());
-        //.toString() function converts to string
-      }
-      console.log(array1[i]);
-      // return array1[i];
-    }
-    //Returns FizzBuzz array
-    return array2;
-  }
-
   const asciiTextRef = useAsciiText({
     font: alphabet,
-    text: `${"Nice"} Hello`,
+    text: "Hello",
     isAnimated: false,
   });
 
@@ -43,7 +16,6 @@ export default function FigletToPage() {
       <p>This a Test</p>
 
       <pre ref={asciiTextRef}></pre>
-      {console.log(FizzBuzz(5))}
     </>
   );
 }
