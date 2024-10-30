5872fb79fb21042f59df35b83dd005a3ed0749bd
AAyinkx <ayinkafinowi@gmail.com>
2024-10-30 09:20:34 +0000

    Trying out figlet animation

---
+++ src/components/figletAnimation.jsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useAsciiText, alphabet } from "react-ascii-text";
+
+export default function FigletAnimation() {
+  //Use ASCII text
+  const asciiTextRef = useAsciiText({
+    font: alphabet,
+    text: " H e l l o ",
+    isAnimated: false,
+  });
+
+  return (
+    <>
+      <p>This a Test</p>
+      <pre ref={asciiTextRef}></pre>
+    </>
+  );
+}
